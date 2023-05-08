0f27e04df13213b43e854d5e83e7767ef449cad3
somrat sorkar <themefisher.dev@gmail.com>
2023-05-08 12:24:53 +0600

    initialize project

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import Footer from "@/partials/Footer";
+import Head from "@/partials/Head";
+import Header from "@/partials/Header";
+import "@/styles/main.scss";
+
+export default function RootLayout({
+  title,
+  meta_title,
+  description,
+  image,
+  noindex,
+  canonical,
+  children,
+}: {
+  title: string;
+  meta_title: string;
+  image: string;
+  canonical: string;
+  noindex: boolean;
+  description: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <Head
+        title={title}
+        meta_title={meta_title}
+        image={image}
+        canonical={canonical}
+        noindex={noindex}
+        description={description}
+      />
+
+      <body suppressHydrationWarning={true}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
