75ec0e69facc431f452236e8623a34fa322e59fe
somrat sorkar <themefisher.dev@gmail.com>
2023-05-13 16:35:41 +0600

    rebuild site

--- src/app/[regular]/page.tsx
+++ src/app/[regular]/page.tsx
@@ -1,6 +1,7 @@
 import Default from "@/layouts/Default";
 import NotFound from "@/layouts/NotFound";
 import { getListPage, getSinglePage } from "@/lib/contentParser";
+import { notFound } from "next/navigation";
 
 export const generateStaticParams = async () => {
   const regularPages = getSinglePage("pages");
@@ -15,8 +16,12 @@ const RegularPages = async ({ params }: { params: { regular: string } }) => {
   const regularData = getSinglePage("pages");
   const NotFoundData = getListPage("pages/404.md");
   const pageData = regularData.filter(
-    (page) => page.slug === params.regular
+    (page) => !page.frontmatter.layout && page.slug === params.regular
   )[0];
+
+  if (!pageData) {
+    notFound();
+  }
 
   return (
     <>
