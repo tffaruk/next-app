b6c194759bfc0cf51e283bcbdd4cdb6d58b169f0
somrat sorkar <themefisher.dev@gmail.com>
2023-05-17 14:15:54 +0600

    testing try catch

--- src/lib/contentParser.ts
+++ src/lib/contentParser.ts
@@ -32,48 +32,58 @@ export const getListPage = (filePath: string) => {
 
 // get all single pages, ex: blog/post.md
 export const getSinglePage = (folder: string) => {
-  const filesPath = fs.readdirSync(path.join(contentPath, folder));
-  const sanitizeFiles = filesPath.filter((file) => file.endsWith(".md"));
-  const filterSingleFiles = sanitizeFiles.filter((file) =>
-    file.match(/^(?!_)/)
-  );
-  const singlePages = filterSingleFiles.map((filename) => {
-    const slug = filename.replace(".md", "");
+  try {
+    const filesPath = fs.readdirSync(path.join(contentPath, folder));
+    const sanitizeFiles = filesPath.filter((file) => file.endsWith(".md"));
+    const filterSingleFiles = sanitizeFiles.filter((file) =>
+      file.match(/^(?!_)/)
+    );
+    const singlePages = filterSingleFiles.map((filename) => {
+      const slug = filename.replace(".md", "");
+  
+      try {
+        const pageData = fs.readFileSync(
+          path.join(contentPath, folder, filename),
+          "utf-8"
+        );
+        const pageDataParsed = matter(pageData);
+        const frontmatterString = JSON.stringify(pageDataParsed.data);
+        const frontmatter = JSON.parse(frontmatterString);
+        const content = pageDataParsed.content;
+        const url = frontmatter.url ? frontmatter.url.replace("/", "") : slug;
+  
+        return {
+          notFound: false,
+          frontmatter: frontmatter,
+          slug: url,
+          content: content
+        };
+      } catch (error) {
+        
+        return {
+          notFound: true,
+          frontmatter: {},
+          slug: "",
+          content: ""
+        };
+      }
+    });
 
-    try {
-      const pageData = fs.readFileSync(
-        path.join(contentPath, folder, filename),
-        "utf-8"
-      );
-      const pageDataParsed = matter(pageData);
-      const frontmatterString = JSON.stringify(pageDataParsed.data);
-      const frontmatter = JSON.parse(frontmatterString);
-      const content = pageDataParsed.content;
-      const url = frontmatter.url ? frontmatter.url.replace("/", "") : slug;
-
-      return { 
-        notFound: false,
-        frontmatter: frontmatter,
-        slug: url,
-        content: content
-      };
-
-    } catch (error) {
-      return { 
-        notFound: true,
-        frontmatter: {},
-        slug: "",
-        content: ""
-      };
-    }
-  });
-
-  const publishedPages = singlePages.filter(
-    (page) => !page.frontmatter.draft && page
-  );
-  const filterByDate = publishedPages.filter(
-    (page) => new Date(page.frontmatter.date || new Date()) <= new Date()
-  );
-
-  return filterByDate;
+    const publishedPages = singlePages.filter(
+      (page) => !page.frontmatter.draft && page
+    );
+    const filterByDate = publishedPages.filter(
+      (page) => new Date(page.frontmatter.date || new Date()) <= new Date()
+    );
+  
+    return filterByDate;
+    
+  } catch (error) {
+    return [{
+      notFound: true,
+      frontmatter: {},
+      slug: "",
+      content: ""
+    }];
+  }
 };
