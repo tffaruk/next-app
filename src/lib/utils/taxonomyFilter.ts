7aadfbae63fa581feb42e739514296cdf1b449a0
somrat sorkar <themefisher.dev@gmail.com>
2023-05-18 10:32:25 +0600

    test

--- src/lib/utils/taxonomyFilter.ts
+++ src/lib/utils/taxonomyFilter.ts
@@ -2,7 +2,7 @@ import { slugify } from "@/lib/utils/textConverter";
 
 const taxonomyFilter = (posts: any[], name: string, key: any) => {
 
-  console.log(posts.length, name, key)
+  console.log(posts)
 
   return posts.filter((post) =>
     post.frontmatter[name].map((name: string) => slugify(name)).includes(key)
